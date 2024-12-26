e51dab9764eb6b53b799854d94dd883c8156eb21
Abdullah Kamal <abdallah10kamal@gmail.com>
2024-12-26 18:46:54 +0200

    fix: routes issues

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ mountRoutes(app)
 const dbconnection = require('./config/dataBase');
 dbconnection()
 
-const port=process.env.PORT||8000
+const port=process.env.PORT||9000
 const server=app.listen(port,() => {
     console.log(`Server is running on port ${port}`);
 });
