dc8f7671da91534a6bf7e60a7377f20b56fe17c3
Abdullah Kamal <abdallah10kamal@gmail.com>
2024-12-28 21:31:01 +0200

    change base url

--- server/index.js
+++ server/index.js
@@ -1,43 +1,41 @@
-const express=require('express')
-const cors=require('cors')
-const https = require('https');
-const app= express();
-const fs=require('fs');
-const dotenv=require('dotenv');
-const mountRoutes=require('./routes');
-
+const express = require("express");
+const cors = require("cors");
+const https = require("https");
+const app = express();
+const fs = require("fs");
+const dotenv = require("dotenv");
+const mountRoutes = require("./routes");
 
 app.use(cors());
-app.options('*', cors());
+app.options("*", cors());
 app.use(express.json());
 
-dotenv.config({path:"config.env"})
-mountRoutes(app)
-const dbconnection = require('./config/dataBase');
-dbconnection()
-
+dotenv.config({ path: "config.env" });
+mountRoutes(app);
+const dbconnection = require("./config/dataBase");
+dbconnection();
 
 const sslOptions = {
-    key: fs.readFileSync('ssl/key.pem'), // Path to your private key
-    cert: fs.readFileSync('ssl/cert.pem'), // Path to your certificate
-  };
+  key: fs.readFileSync("ssl/nginx-selfsigned.key"), // Path to your private key
+  cert: fs.readFileSync("ssl/nginx-selfsigned.crt"), // Path to your certificate
+};
 
-const port=process.env.PORT||9000
-const server = https.createServer(sslOptions, app).listen(port, '0.0.0.0', () => {
+const port = process.env.PORT || 9000;
+const server = https
+  .createServer(sslOptions, app)
+  .listen(port, "0.0.0.0", () => {
     console.log(`HTTPS Server is running on https://0.0.0.0:${port}`);
-});
-
+  });
 
 // const server=app.listen(port,() => {
 //     console.log(`Server is running on port ${port}`);
 // });
 
 //Handel Rejection outside of express
-process.on("unhandledRejection",(err)=>{
-    console.error(`unhandledRejection Errors : ${err.name}|${err.message}`);
-    server.close(()=>{
-        console.error(`Shutting down ....`)
-        process.exit(1);
-    })
-    
-})
+process.on("unhandledRejection", (err) => {
+  console.error(`unhandledRejection Errors : ${err.name}|${err.message}`);
+  server.close(() => {
+    console.error(`Shutting down ....`);
+    process.exit(1);
+  });
+});
